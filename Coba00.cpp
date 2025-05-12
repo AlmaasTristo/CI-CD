d1478b63358715107566e3972494029bff07730c
Almaas Tristo <almaas@students.amikom.ac.id>
2025-05-12 14:00:23 +0700

    commit 1

---
+++ Coba00.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2;
+    char op;
+
+    cout << "Kalkulator Sederhana\n";
+    cout << "Masukkan dua angka: ";
+    cin >> num1 >> num2;
+
+    cout << "Pilih operator (+, -, *, /): ";
+    cin >> op;
+
+    switch (op) {
+        case '+':
+            cout << "Penjumlahan: " << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+            break;
+        case '-':
+            cout << "Hasil: " << num1 - num2 << endl;
+            break;
+        case '*':
+            cout << "Hasil: " << num1 * num2 << endl;
+            break;
+        case '/':
+            if (num2 != 0)
+                cout << "Hasil: " << num1 / num2 << endl;
+            else
+                cout << "Error: Pembagian dengan nol!" << endl;
+            break;
+        default:
+            cout << "Operator tidak valid!" << endl;
+    }
+
+    return 0;
+}
